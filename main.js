6ad886ca8a88b080b267170a4925d8ef345b38fe
Maylon-tech <minatokiroisen77@gmail.com>
2023-02-20 09:52:14 +0900

    cronometro layout

---
+++ main.js
@@ -0,0 +1,22 @@
+const minutesElem = document.querySelector("#minutes")
+const secondsElem = document.querySelector("#seconds")
+const millisecondsElem = document.querySelector("#milliseconds")
+const startBtn = document.querySelector("#startBtn")
+const pauseBtn = document.querySelector("#pauseBtn")
+const resumeBtn = document.querySelector("#resumeBtn")
+const resetBtn = document.querySelector("#resetBtn")
+
+let interval
+let minutes = 0
+let seconds = 0
+let milliseconds = 0
+let isPaused = false
+
+
+startBtn.addEventListener("click", startTimer)
+
+function startTimer() {
+
+
+    interval = setInterval(() = {})
+}
